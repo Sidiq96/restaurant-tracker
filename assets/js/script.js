45e45a998ada5473fb684f10e7afaafeeba1dd18
Suhaim Khalid <suhaimkhalid007@gmail.com>
2023-12-04 23:40:00 +0000

    Add Function to show restaurant data dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,53 +94,136 @@ $(document).ready(function () {
     performSearch(`restaurants in ${location}, London`);
   });
 
-  // Function to fetch restaurant details
-  function fetchRestaurant(restaurantName) {
-    const url = `https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=${encodeURIComponent(restaurantName)}&currencyCode=GBR&rapidapi-key=73a6b7e941msh7b32adeb46d86d8p18b277jsn70c5ae8fc490`;
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        updateRestaurantCard(data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+});
+
+
+
+
+  // Suhaim Code
+  // Function to fetch restaurant details
+  fetchRestaurant();
+  function fetchRestaurant() {
+
+  var apiKey="eec2014a26msh690cd1a430a3f1dp18ead0jsn58b826e0a4d7";
+
+  var url = "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=Restaurant_Review-g304554-d8010527-Reviews-Saptami-Mumbai_Maharashtra&currencyCode=USD&rapidapi-key="+ apiKey;
+
+
+  fetch(url).then(function(response){
+  return response.json();
+  })
+  .then(function(data){
+  console.log(data);
+    display_restaurant_html(data);
+  })
+  .catch(function(error){
+  console.error(error)
+  });
   }
 
+
+  // Suhaim Code 
   // Function to update restaurant card UI
-  function updateRestaurantCard(details) {
-    // Select the container where the restaurant card will be appended
-    const restaurantCard = $('#restaurant_section');
+  function display_restaurant_html(details) {
+  var restaurant = details.data;
+  console.log(restaurant);
+  // Select the container where the restaurant card will be appended
+  var restaurantsection = $('.restaurant_section');
 
-    // Clear previous content in the container
-    restaurantCard.empty();
+  // Clear previous content in the container
+  restaurantsection.empty();
 
-    // Check if details object has necessary properties
-    if (details && details.overview && details.location) {
-      const restaurantName = details.overview.name;
-      const restaurantDes = details.location.neighborhood_info[0];
+  // Getting value from the api
+  var restaurantName = restaurant.location.name;
+  var restaurant_address = restaurant.location.address;
+  var restaurantDes = restaurant.location.description;
 
-      // Append a new restaurant card to the container
-      restaurantCard.append(`
-        <div class="container">
-          <div class="row">
-            <div class="col-4">
-              <div class="restaurant_card">
-                <div class="restaurant_body">
-                  <h3 class="rest_name">${restaurantName}</h3>
-                  <p class="rest_des">${restaurantDes}</p>
-                  <p class="feedback_text">FeedBack</p>
-                  <input type="text" id="feedback" class="form-control" />
-                </div>
-              </div>
+  // For In hours string remove today
+  var restaurant_hours = restaurant.hours.hoursTodayText;
+  let originalString = restaurant_hours;
+
+// Replace "Today" with an empty string
+  let modifiedString = originalString.replace("Today", "");
+  var restaurant_hours = modifiedString;
+  var restaurant_number = restaurant.location.phone;
+  var restaurant_website =  restaurant.location.website;
+  var restaurant_photo = restaurant.location.photo.images.original.url;
+
+
+  // Append a new restaurant card to the container
+  restaurantsection.append(`
+  <div class="container">
+  <h2 class="rest_title">Restaurant Details</h2>
+    <!-- Restaurant card -->
+    <div class="restaurant_card">
+      <div class="row">
+        <div class="col-md-8 col-12">
+          <div class="restaurant_left_side">
+            <!-- Restaurant name -->
+            <h3 class="restaurant_name">${restaurantName}</h3>
+            <p class="restaurant_address">${restaurant_address}</p>
+            <p class="restaurant_description">${restaurantDes}</p>
+            <ul class="restaurant_ul">
+                <li><p class="restaurant_hours">${restaurant_hours}</p></li>
+                <li><p class="restaurant_number">${restaurant_number}</p></li>
+                <li><p class="restaurant_website"><a href="${restaurant_website}">Website</a></p></li>
+            </ul>
+            <div class="resturant_feedback_Section">
+              <h4 class="FeedBack_title">Feedback</h4>
+              <textarea name="" id="FeedBack_text"></textarea>
+              <button class="btn btn-lg" id="submit_feedback">Submit</button>
             </div>
           </div>
         </div>
-      `);
-    } else {
-      console.error("Invalid details object:", details);
-    }
+        <div class="col-md-4 col-12">
+          <div class="restaurant_right_side">
+            <img src="${restaurant_photo}" alt="Restaurant Image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `);
+
   }
-});
+
+  
+  // Suhaim Code
+  // =============================== Local Storage on Submit Button Code ==================== 
+
+  // Function TO save data in local Storage
+  $(".restaurant_section").on("click","#submit_feedback",function(event){
+  event.preventDefault();
+  console.log("submit");
+  var restaurant_name = $(".restaurant_name");
+  var restaurant_description = $(".restaurant_description");
+  var restaurant_hours = $(".restaurant_hours");
+  var restaurant_website = $(".restaurant_website");
+  var restaurant_photo = $(".restaurant_photo");
+
+  var FeedBack_text= $("#FeedBack_text");
+
+ // Create an object to represent the current feedback
+ var feedbackData = {
+  name: restaurant_name,
+  description: restaurant_description,
+  hours: restaurant_hours,
+  website: restaurant_website,
+  photo: restaurant_photo,
+  feedback: FeedBack_text,
+};
+
+var Json_String = JSON.stringify(feedbackData);
+localStorage.setItem("Restaurants",Json_String);
+
+})
+
+
+
+
+
+
+
+
+  
